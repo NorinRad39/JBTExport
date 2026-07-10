f8aff8a45149a6202a9d1e0031c0142e7a805a4b
Florent FABBRI <norinrad.39@gmail.com>
2026-07-10 15:09:26 +0200

    a suivre

--- JBTExport/Program.cs
+++ JBTExport/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TopSolid.Kernel.Automating;
+using TSH = TopSolid.Kernel.Automating.TopSolidHost;
+using OutilsTs;
 
 namespace JBTExport
 {
@@ -10,6 +13,51 @@ namespace JBTExport
     {
         static void Main(string[] args)
         {
+            var connector = new StartConnect();
+
+            connector.ConnectionTopsolid();
+
+            if (TSH.IsConnected)
+            {
+                Console.WriteLine("TSH is connected");
+            }
+
+            FindExporterIndexByExtension("Pdf", out int exporterIndex);
+            Console.WriteLine($"Exporter index for PDF: {exporterIndex}");
+            List<KeyValue> options = TSH.Application.GetExporterOptions(exporterIndex);
+            foreach (KeyValue kv in options)
+            {
+                Console.WriteLine($"Option: {kv.Key} = {kv.Value}");
+            }
         }
+
+            private static bool FindExporterIndexByExtension(string extension, out int exporterIndex)
+            {
+                exporterIndex = -1;
+
+                for (int i = 0; i < TSH.Application.ExporterCount; i++)
+                {
+                    TSH.Application.GetExporterFileType(i, out string fileTypeName, out string[] outFileExtensions);
+
+                    // Vérifier si l'extension existe dans le tableau
+                    if (outFileExtensions != null && outFileExtensions.Any(ext => ext.TrimStart('.').Equals(extension, StringComparison.OrdinalIgnoreCase)))
+                    {
+                        exporterIndex = i;
+                        return true;
+                    }
+                }
+
+                return false;
+            }
+
     }
+
+
 }
+
+
+
+
+
+
+
